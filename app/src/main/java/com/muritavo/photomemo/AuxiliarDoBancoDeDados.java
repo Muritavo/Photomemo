ea86624b0e7853207bc330a7f4dcbbb5e673ac94
Murilo Araujo <muritavo@outlook.com>
2015-08-28 19:05:07 -0300

    Implementação do método que carrega as imagens

--- app/src/main/java/com/muritavo/photomemo/AuxiliarDoBancoDeDados.java
+++ app/src/main/java/com/muritavo/photomemo/AuxiliarDoBancoDeDados.java
@@ -1,7 +1,26 @@
 package com.muritavo.photomemo;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by 15111018 on 28/08/2015.
  */
-public class AuxiliarDoBancoDeDados {
+public class AuxiliarDoBancoDeDados extends SQLiteOpenHelper{
+    public AuxiliarDoBancoDeDados(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createCommand = "CREATE TABLE Photomemo (_id integer primary key autoincrement, identificador integer, FOREIGN KEY (identificador) REFERENCES imagens (_id), descricao TEXT);";
+
+        db.execSQL(createCommand);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
